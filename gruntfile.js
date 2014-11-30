6c3ab0146bf783ee4a75d434dbb4a094a8af5f50
Ramesh Polishetti <ramesh.polishetti@macys.com>
2014-11-30 11:53:53 +0530

    Added html beautifier settings.

--- gruntfile.js
+++ gruntfile.js
@@ -233,18 +233,27 @@ module.exports = function( grunt ) {
             options: {
                 //config: "path/to/configFile",
                 html: {
+                    allowedFileExtensions: [ "htm", "html", "xhtml", "shtml", "xml", "svg" ],
                     braceStyle: "collapse",
+                    endWithNewline: false,
                     indentChar: " ",
+                    indentHandlebars: false,
+                    indentInnerHtml: true,
                     indentScripts: "keep",
                     indentSize: 4,
-                    maxPreserveNewlines: 10,
+                    maxPreserveNewlines: 2,
                     preserveNewlines: true,
-                    unformatted: [ "a", "sub", "sup", "b", "i", "u" ],
-                    wrapLineLength: 0
+                    unformatted: [ "a", "span", "img", "code", "pre", "sub", "sup", "em", "strong", "b", "i", "u", "strike", "big", "small", "pre", "h1", "h2", "h3", "h4", "h5", "h6" ],
+                    wrapLineLength: 0,
+                    removeExtraline: true
                 },
                 css: {
+                    allowedFileExtensions: [ "css", "scss", "sass", "less" ],
+                    endWithNewline: false,
                     indentChar: " ",
-                    indentSize: 4
+                    indentSize: 4,
+                    selectorSeparator: " ",
+                    selectorSeparatorNewline: false
                 },
                 js: {
                     braceStyle: "collapse",
